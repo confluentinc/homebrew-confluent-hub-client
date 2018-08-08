dc62552aa5f34e8fe1b7abd3629ca4a7483e8a50
Alex Diachenko <sansanichfb@gmail.com>
2018-08-07 17:07:51 -0700

    CC-2059: Use brew cask.

---
+++ Casks/confluent-hub-client.rb
@@ -0,0 +1,23 @@
+cask 'confluent-hub-client' do
+  version '5.1.0-SNAPSHOOT'
+  sha256 'a0c008f95cd3cd01b3a24729a27307ac5d01990cd9b73b185ccfb0728677af80'
+
+  url "https://s3-us-west-2.amazonaws.com/confluent-hub-client/ConfluentHubClient-#{version}.pkg"
+  name 'Confluent Hub Client'
+  homepage 'https://www.confluent.io/hub/'
+  pkg "ConfluentHubClient#{version}.pkg"
+
+   postflight do
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Applications/ConfluentHubClient.app/Contents/bin/confluent-hub", '/usr/local/bin/confluent-hub'],
+                   sudo: true
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Applications/ConfluentHubClient.app/Contents/share/java/confluent-hub-client", '/usr/local/share/java/confluent-hub-client'],
+                   sudo: true
+  end
+
+  uninstall delete:    [
+                         '/usr/local/bin/confluent-hub',
+                         '/usr/local/share/java/confluent-hub-client',
+                       ]
+end
