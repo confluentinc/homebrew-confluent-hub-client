40c97a0b4d6e5e0cfe26fa697e3ce5fcd31e59dc
Alex Diachenko <sansanichfb@gmail.com>
2018-08-09 18:02:31 -0700

    CC-2059: Use client.hub.confluent.io domain.

--- Casks/confluent-hub-client.rb
+++ Casks/confluent-hub-client.rb
@@ -1,7 +1,7 @@
 cask 'confluent-hub-client' do
   version '5.0.1-SNAPSHOT'
   sha256 '3bf8a99e08b2dafe91ea6c114c1960f8cedc57c3df8a0bce77d01a4c68dc11d3'
-  url "https://s3-us-west-2.amazonaws.com/confluent-hub-client/confluent-hub-client-#{version}-package.tar.gz"
+  url "http://client.hub.confluent.io/confluent-hub-client-#{version}-package.tar.gz"
   name 'Confluent Hub Client'
   homepage 'https://www.confluent.io/hub/'
   binary "#{staged_path}/bin/confluent-hub"
