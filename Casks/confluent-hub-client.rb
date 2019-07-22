48a0bab245e6cf2ae95035d21a2b47e3eb881504
Damien Nozay <dnozay@confluent.io>
2019-07-22 15:06:18 -0700

    Fix version 5.3.0-rc4 => 5.3.0

--- Casks/confluent-hub-client.rb
+++ Casks/confluent-hub-client.rb
@@ -1,5 +1,5 @@
 cask 'confluent-hub-client' do
-  version '5.3.0-rc4'
+  version '5.3.0'
   sha256 '8c092e64cb230b4ab0fffe3f50d684d4f4621c822e510630ee3afd2fb65adfc5'
   url "http://client.hub.confluent.io/confluent-hub-client-#{version}-package.tar.gz"
   name 'Confluent Hub Client'
