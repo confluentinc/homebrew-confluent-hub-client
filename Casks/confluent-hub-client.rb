3a7df207311bba750bdc40ae7c2b4d22833a3cfc
venkatteki <76439278+venkatteki@users.noreply.github.com>
2022-06-21 23:07:43 +0530

    Bump up formula to v7.1.1 (#27)
    Co-authored-by: Confluent Jenkins Bot <jenkins@confluent.io>

--- Casks/confluent-hub-client.rb
+++ Casks/confluent-hub-client.rb
@@ -1,6 +1,6 @@
 cask 'confluent-hub-client' do
-  version '7.0.2'
-  sha256 '28822e3c8ffb6d7c18cce883de3c0d77ab68ff329e81f488331fa63da244ff87'
+  version '7.1.1'
+  sha256 'fd7aa849bbd547996c6272efade3cb50e5affcb30766b85479f977be12286ae2'
   url "http://client.hub.confluent.io/confluent-hub-client-#{version}-package.tar.gz"
   name 'Confluent Hub Client'
   homepage 'https://www.confluent.io/hub/'
