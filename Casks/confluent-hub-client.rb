d3958145584afb66a2bebaa05230c3b3ea2a64d0
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-11-29 09:33:22 +0000

    Bump up formula to v7.7.2

--- Casks/confluent-hub-client.rb
+++ Casks/confluent-hub-client.rb
@@ -1,6 +1,6 @@
 cask 'confluent-hub-client' do
-  version '7.3.4'
-  sha256 '944ef121c257ebf44905df1bc8de81d2d140ac158e66d8ce6379752ac24ea9a6'
+  version '7.7.2'
+  sha256 'e05ad43dc5c773708841fc1ea256e6f1f184bb0024746b11d4482950f8e05ad8'
   url "http://client.hub.confluent.io/confluent-hub-client-#{version}-package.tar.gz"
   name 'Confluent Hub Client'
   homepage 'https://www.confluent.io/hub/'
